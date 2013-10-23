0242cf825a74a54c7f042f9862e1112024ef0b23
countxyz <skeptoid@gmail.com>
2013-10-23 02:02:52 -0400

    Modify routes and make MainPages#home root

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Harmonize::Application.routes.draw do
-  get "main_pages/home"
+
+  root 'main_pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
