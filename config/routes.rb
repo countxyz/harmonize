f3bef1ec7f5130940b711422206a0ad209aa2005
countxyz <skeptoid@gmail.com>
2013-10-23 11:01:14 -0400

    Add index and show views for project controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Harmonize::Application.routes.draw do
 
   root 'main_pages#home'
+  resources :projects
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
