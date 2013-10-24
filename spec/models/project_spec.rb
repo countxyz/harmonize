986fa60faa64952b9bf21470bced76c2acc7b658
countxyz <skeptoid@gmail.com>
2013-10-23 20:12:10 -0400

    Require that a name be provided for a project

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,12 +1,24 @@
 require 'spec_helper'
 
 describe Project do
-
-	before do
-		@project = Project.new(name: "a")
+	
+	it "is valid with a name, status, priority, start_date,
+		target_date, deadline, completion_date, and notes" do
+		project = Project.new(
+			name: 						'a',
+			status: 					'a',
+			priority: 				'a',
+			start_date: 			'0000-00-00',
+			target_date: 			'0000-00-00',
+			deadline: 				'0000-00-00',
+			completion_date: 	'0000-00-00',
+			notes: 						'a')
+		expect(project).to be_valid
 	end
 
-	subject { @project }
-
-	expect(@project).to be_valid
+	it "is invalid without a name" do
+		expect(Project.new(name: nil)).to have(1).errors_on(:name)
+	end
+	
+	it "is invalid without a status"
 end
