c98be8af45a7af5f44fdadcde609ce4aaa930ec6
countxyz <skeptoid@gmail.com>
2013-11-06 15:01:46 -0500

    Back-write controller specs for Project index

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,16 +1,23 @@
 require 'spec_helper'
 
 describe ProjectsController do
+
 	describe "GET #index" do
-		it "responds successfully with an HTTP 200 status code" do
+		it "has a 200 status code" do
 			get :index
-			expect(response).to be_success
 			expect(response.status).to eq(200)
 		end
 
 		it "renders the index template" do
 			get :index
 			expect(response).to render_template("index")
+		end
+
+		it "loads all projects into @projects" do
+			project_1 = Project.create(name: '1', status: 'Completed', priority: 'N/A')
+			project_2 = Project.create(name: '2', status: 'Completed', priority: 'N/A')
+			get :index
+			expect(assigns(:projects)).to match_array([project_1, project_2])
 		end
 	end
 end
--- spec/views/main_pages/home.html.erb_spec.rb
+++ spec/views/main_pages/home.html.erb_spec.rb
@@ -1,2 +1,5 @@
 require 'spec_helper'
+
+describe "Home" do
+
 end
