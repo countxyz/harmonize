e2085e71ab0e0d1248f2cfa091ab8ff80624b43b
countxyz <skeptoid@gmail.com>
2013-11-08 21:19:05 -0500

    Refactor main pages feature spec. Dry spec by adding a before each hook to visit / page

--- spec/features/main_pages_spec.rb
+++ spec/features/main_pages_spec.rb
@@ -3,14 +3,15 @@ require 'spec_helper'
 describe "Main pages" do
   
   describe "Home page" do
+    before :each do
+      visit '/'
+    end
 
     it "has the content 'Harmonize'" do
-      visit '/'
       expect(page).to have_content('Harmonize')
     end
 
     it "has title 'Harmonize | Home'" do
-      visit '/'
       expect(page).to have_title("Harmonize | Home")
     end
   end
