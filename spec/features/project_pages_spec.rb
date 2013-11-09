659f4689c986b30917042a5fa8ef5afa255affb3
countxyz <skeptoid@gmail.com>
2013-11-09 04:53:58 -0500

    Back-write spec to ensure Project index title - All Projects

--- spec/features/project_pages_spec.rb
+++ spec/features/project_pages_spec.rb
@@ -9,6 +9,11 @@ describe "Project pages" do
       expect(page).to have_title("Harmonize | All Projects")
     end
 
+    it "has h2 'All Projects'" do
+      page_title = find('#main_content').find('h2')
+      expect(page_title).to have_content('All Projects')
+    end
+
     it "has link to Projects new project page" do
       find('#main_content').click_link('New Project')
       expect(current_path).to eq new_project_path
