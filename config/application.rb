b77df64b616e41156da43d824c6c330297d8e75a
countxyz <skeptoid@gmail.com>
2013-11-19 22:31:24 -0500

    Configure assets to be compiled by Heroku and remove manifest file.

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,5 +22,6 @@ module Harmonize
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.initialize_on_precompile = false
   end
 end
