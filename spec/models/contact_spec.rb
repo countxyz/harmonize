3660edabe105235c0f2bb427c25fbbeec91aba73
countxyz <skeptoid@gmail.com>
2013-11-23 17:57:55 -0500

    Verify what contact model looks like when valid.

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Contact do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a first name, last name, email, and phone number" do
+    contact = Contact.new(
+      first_name:   'Bill',
+      last_name:    'Burr',
+      email:        'bill@billburr.com',
+      phone: '(555) 555 - 5555')
+    expect(contact).to be_valid
+  end
+
+  it "is invalid without a first name"
+  it "is invalid without a last name"
+  it "is invalid without an email address"
 end
