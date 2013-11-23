c3e5f0942a081112482edeb1ef9dfdf3024883e7
countxyz <skeptoid@gmail.com>
2013-11-23 00:34:48 -0500

    Provide interface to view and create contacts.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,33 @@
 class ContactsController < ApplicationController
+
+  def index
+    @contacts = Contact.all
+  end
+
+  def show
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(project_params)
+    if @contact.save
+      flash[:notice] = "Contact has been created."
+      redirect_to @contact
+    else
+      flash[:alert] = "Contact has not been created."
+      render :action => "new"
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def project_params
+    params.require(:contact).permit(:first_name, :last_name, :email, :phone)
+  end
 end
