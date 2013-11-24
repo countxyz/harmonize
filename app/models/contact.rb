fe346deac018c7d9fbbad22e79e9b9079e7a3a8d
countxyz <skeptoid@gmail.com>
2013-11-23 21:21:28 -0500

    Validate email format.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates :first_name, :last_name, :email, presence: true
+  validates :email, format: { with: VALID_EMAIL_REGEX }
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe Contact do
+
+  before(:each) do 
+    @contact = Contact.new(first_name: "a", last_name: "a", 
+                           email: "contact@target.com")
+  end
+
   it "is valid with a first name, last name, email, and phone number" do
     contact = Contact.new(
       first_name:   'Bill',
@@ -18,6 +24,26 @@ describe Contact do
   end
 
   it "is invalid without an email address" do
-    expect(Contact.new(email: nil)).to have(1).errors_on(:email)
+    expect(Contact.new(email: nil)).to_not be_valid
+  end
+
+  describe "#email" do
+    it "is valid with the format" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp
+                     a+b@baz.cn]
+      addresses.each do |valid_address|
+        @contact.email = valid_address
+        expect(@contact).to be_valid
+      end
+    end
+    
+    it "is invalid with the format" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @contact.email = invalid_address
+        expect(@contact).not_to be_valid
+      end
+    end
   end
 end
