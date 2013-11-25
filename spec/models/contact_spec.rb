5c4c4febda2a15a1746d7d7ffdb7274ac4b844ee
countxyz <skeptoid@gmail.com>
2013-11-24 22:41:08 -0500

    Refactor contact specs.

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,72 +1,56 @@
 require 'spec_helper'
 
 describe Contact do
-
-  before(:each) do 
-    @contact = Contact.new(first_name: "a", last_name: "a", 
-                           email: "contact@target.com")
-  end
-
   it "has a valid factory" do
     expect(build(:contact)).to be_valid
   end
 
-  it "is valid with a first name, last name, email, and phone number" do
-    contact = Contact.new(
-      first_name:   'Bill',
-      last_name:    'Burr',
-      email:        'bill@billburr.com')
-    expect(contact).to be_valid
-  end
-
-  describe "#first_name" do
-    it "is invalid when not provided" do
-      expect(Contact.new(first_name: nil)).to have(1).errors_on(:first_name)
+  describe "empty fields" do
+    it "is invalid without a first name" do
+      expect(build(:contact, first_name: nil)).to_not be_valid
     end
 
-    it "is invalid when it has more than 30 characters" do
-      @contact.first_name = 'a' * 31
-      expect(@contact).to_not be_valid
-    end   
-  end
-
-  describe "#last_name" do
-    it "is invalid when not provided" do
-      expect(Contact.new(last_name: nil)).to have(1).errors_on(:last_name)
+    it "is invalid without a last name" do
+      expect(build(:contact, last_name: nil)).to_not be_valid
     end
 
-    it "is invalid when it has more than 30 characters" do
-      @contact.last_name = 'a' * 31
-      expect(@contact).to_not be_valid
+    it "is invalid without an email address" do
+      expect(build(:contact, email: nil)).to_not be_valid
+    end
+
+    it "is valid without a phone" do
+      expect(build(:contact, phone: nil)).to be_valid
     end
   end
 
-  it "is invalid without an email address" do
-    expect(Contact.new(email: nil)).to_not be_valid
+  describe "field lengths" do
+    it "is invalid when first name has more than 30 characters" do
+      expect(build(:contact, first_name: 'a' * 31)).to_not be_valid
+    end
+
+    it "is invalid when last name has more than 30 characters" do
+      expect(build(:contact, last_name: 'a' * 31)).to_not be_valid
+    end
+
+    it "is invalid when email has more than 50 characters" do
+      expect(build(:contact, email: 'a' * 45 + "@a.com")).to_not be_valid
+    end
   end
 
-  describe "#email" do
-    it "is valid with the format" do
-      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp
-                     a+b@baz.cn]
-      addresses.each do |valid_address|
-        @contact.email = valid_address
-        expect(@contact).to be_valid
-      end
-    end
-    
-    it "is invalid with the format" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
+  describe "format" do
+
+    it "is invalid with an incorrect email format" do
+      addresses = %w[a@foo,com a_at_foo.org a.b@foo. a@b_baz.com foo@b+baz.com]
       addresses.each do |invalid_address|
-        @contact.email = invalid_address
-        expect(@contact).not_to be_valid
+        expect(build(:contact, email: invalid_address)).to_not be_valid
       end
     end
 
-    it "is invalid when it has more than 50 characters" do
-      @contact.email = 'a' * 45 + "@a.com"
-      expect(@contact).to_not be_valid
+    it "is valid with a correct email format" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        expect(build(:contact, email: valid_address)).to be_valid
+      end
     end
   end
 end
