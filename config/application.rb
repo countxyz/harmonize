f9d6d35c42cc06a87d0ef3002652d3306b3b2c96
countxyz <skeptoid@gmail.com>
2013-11-25 10:15:00 -0500

    Configure RSpec generators.

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,18 @@ module Harmonize
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.initialize_on_precompile = false
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false,
+        feature_specs: true,
+        model_specs: true
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
