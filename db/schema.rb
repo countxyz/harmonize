6efab84175b7bb04dc47781a47bbe75c29cb7625
countxyz <skeptoid@gmail.com>
2013-12-01 15:41:31 -0500

    Remove column voip from phones table.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,17 @@
-ActiveRecord::Schema.define(version: 20131126192355) do
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20131201203614) do
 
   create_table "contacts", force: true do |t|
     t.string   "first_name"
@@ -14,7 +27,6 @@ ActiveRecord::Schema.define(version: 20131126192355) do
     t.integer  "contact_id"
     t.string   "mobile"
     t.string   "office"
-    t.string   "voip"
     t.string   "fax"
     t.string   "home"
     t.datetime "created_at"
