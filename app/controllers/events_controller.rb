ba8e72b26f57915c6976b812af8252ea0be68729
countxyz <skeptoid@gmail.com>
2013-12-24 12:09:54 -0500

    Refactor Events controller.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -23,7 +23,8 @@ class EventsController < ApplicationController
   
   
   def get_events
-    @events = Event.find(:all, :conditions => ["starttime >= '#{Time.at(params['start'].to_i).to_formatted_s(:db)}' and endtime <= '#{Time.at(params['end'].to_i).to_formatted_s(:db)}'"] )
+    @events = Event.find(:all, :conditions => [
+      "starttime >= '#{Time.at(params['start'].to_i).to_formatted_s(:db)}' and endtime <= '#{Time.at(params['end'].to_i).to_formatted_s(:db)}'"] )
     events = [] 
     @events.each do |event|
       events << {:id => event.id, :title => event.title, :description => event.description || "Some cool description here...", :start => "#{event.starttime.iso8601}", :end => "#{event.endtime.iso8601}", :allDay => event.all_day, :recurring => (event.event_series_id)? true: false}
@@ -34,8 +35,10 @@ class EventsController < ApplicationController
   def move
     @event = Event.find_by_id params[:id]
     if @event
-      @event.starttime = (params[:minute_delta].to_i).minutes.from_now((params[:day_delta].to_i).days.from_now(@event.starttime))
-      @event.endtime = (params[:minute_delta].to_i).minutes.from_now((params[:day_delta].to_i).days.from_now(@event.endtime))
+      @event.starttime = (params[:minute_delta].to_i).minutes.from_now(
+        (params[:day_delta].to_i).days.from_now(@event.starttime))
+      @event.endtime = (params[:minute_delta].to_i).minutes.from_now(
+        (params[:day_delta].to_i).days.from_now(@event.endtime))
       @event.all_day = params[:all_day]
       @event.save
     end
@@ -44,26 +47,29 @@ class EventsController < ApplicationController
   
   
   def resize
-    @event = Event.find_by_id params[:id]
+    @event = Event.find(params[:id])
     if @event
-      @event.endtime = (params[:minute_delta].to_i).minutes.from_now((params[:day_delta].to_i).days.from_now(@event.endtime))
+      @event.endtime = (params[:minute_delta].to_i).minutes.from_now(
+        (params[:day_delta].to_i).days.from_now(@event.endtime))
       @event.save
     end    
     render :nothing => true
   end
   
   def edit
-    @event = Event.find_by_id(params[:id])
+    @event = Event.find(params[:id])
     render :json => { :form => render_to_string(:partial => 'edit_form') } 
   end
   
   def update
-    @event = Event.find_by_id(params[:event][:id])
+    @event = Event.find(params[:event][:id])
     if params[:event][:commit_button] == "Update All Occurrence"
       @events = @event.event_series.events
       @event.update_events(@events, event_params)
     elsif params[:event][:commit_button] == "Update All Following Occurrence"
-      @events = @event.event_series.events.find(:all, :conditions => ["starttime > '#{@event.starttime.to_formatted_s(:db)}' "])
+      @events = @event.event_series.events.find(
+        :all, :conditions => [
+        "starttime > '#{@event.starttime.to_formatted_s(:db)}' "])
       @event.update_events(@events, event_params)
     else
       @event.attributes = event_params
@@ -77,7 +83,9 @@ class EventsController < ApplicationController
     if params[:delete_all] == 'true'
       @event.event_series.destroy
     elsif params[:delete_all] == 'future'
-      @events = @event.event_series.events.find(:all, :conditions => ["starttime > '#{@event.starttime.to_formatted_s(:db)}' "])
+      @events = @event.event_series.events.find(
+        :all, :conditions => [
+        "starttime > '#{@event.starttime.to_formatted_s(:db)}' "])
       @event.event_series.events.delete(@events)
     else
       @event.destroy
@@ -87,7 +95,9 @@ class EventsController < ApplicationController
 
   private
     def event_params
-      params.require(:event).permit('title', 'description', 'starttime(1i)', 'starttime(2i)', 'starttime(3i)', 'starttime(4i)', 'starttime(5i)', 'endtime(1i)', 'endtime(2i)', 'endtime(3i)', 'endtime(4i)', 'endtime(5i)', 'all_day', 'period', 'frequency', 'commit_button')
-    end
-  
+      params.require(:event).permit('title', 'description', 'starttime(1i)', 
+        'starttime(2i)', 'starttime(3i)', 'starttime(4i)', 'starttime(5i)', 
+        'endtime(1i)', 'endtime(2i)', 'endtime(3i)', 'endtime(4i)', 
+        'endtime(5i)', 'all_day', 'period', 'frequency', 'commit_button')
+    end  
 end
