fd4ae3305a8f2da4974ede2bebeee6797e1efdf8
countxyz <skeptoid@gmail.com>
2013-12-24 05:02:37 -0500

    Create Events controller.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,47 @@
 class Event < ActiveRecord::Base
+  attr_accessor :period, :frequency, :commit_button
+  
+  validates_presence_of :title, :description
+  validate :validate_timings
+  
+  belongs_to :event_series
+  
+  REPEATS = [
+              "Does not repeat",
+              "Daily"          ,
+              "Weekly"         ,
+              "Monthly"        ,
+              "Yearly"         
+  ]
+  
+  def validate_timings
+    if (starttime >= endtime) and !all_day
+      errors[:base] << "Start Time must be less than End Time"
+    end
+  end
+  
+  def update_events(events, event)
+    events.each do |e|
+      begin 
+        st, et = e.starttime, e.endtime
+        e.attributes = event
+        if event_series.period.downcase == 'monthly' or event_series.period.downcase == 'yearly'
+          nst = DateTime.parse("#{e.starttime.hour}:#{e.starttime.min}:#{e.starttime.sec}, #{e.starttime.day}-#{st.month}-#{st.year}")  
+          net = DateTime.parse("#{e.endtime.hour}:#{e.endtime.min}:#{e.endtime.sec}, #{e.endtime.day}-#{et.month}-#{et.year}")
+        else
+          nst = DateTime.parse("#{e.starttime.hour}:#{e.starttime.min}:#{e.starttime.sec}, #{st.day}-#{st.month}-#{st.year}")  
+          net = DateTime.parse("#{e.endtime.hour}:#{e.endtime.min}:#{e.endtime.sec}, #{et.day}-#{et.month}-#{et.year}")
+        end
+      rescue
+        nst = net = nil
+      end
+      if nst and net
+        e.starttime, e.endtime = nst, net
+        e.save
+      end
+    end
+    
+    event_series.attributes = event
+    event_series.save
+  end
 end
