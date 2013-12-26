da3f7c20794079fa448818807e832aa32f7ca081
countxyz <skeptoid@gmail.com>
2013-12-25 19:08:25 -0500

    Modify validate_time in Event class to reflect time changes in Events table.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < ActiveRecord::Base
   
   validates_presence_of :title, :description
-  validate :validate_timings
+  validate :validate_time
   
-  def validate_timings
-    if (starttime >= endtime) and !all_day
+  def validate_time
+    if (start_time >= end_time) and !all_day
       errors[:base] << "Start Time must be before End Time"
     end
   end
