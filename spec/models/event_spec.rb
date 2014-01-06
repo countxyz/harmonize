4b5e49745d3bc40d3819925c234f6ca7c6fb6856
countxyz <skeptoid@gmail.com>
2014-01-06 16:47:29 -0500

    Back-write missing Event model specs.

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,4 +1,27 @@
 require 'spec_helper'
 
 describe Event do
+  let(:event) { FactoryGirl.create :event }
+
+  it 'has a valid factory' do
+    expect(create(:event)).to be_valid
+  end
+
+  describe 'empty fields' do
+    it 'is invalid when title is not provided' do
+      expect(build(:event, title: nil)).to_not be_valid
+    end
+
+    it 'is invalid when description is not provided' do
+      expect(build(:event, description: nil)).to_not be_valid
+    end
+  end
+
+  describe 'invalid dates' do
+    it 'cannot have an end_time before start_time' do
+      event = build(:event, start_time: '2014-01-24 01:00:00', 
+                            end_time: '2014-01-23 01:00:00')
+      expect(event).to_not be_valid
+    end
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,60 +3,60 @@ require 'spec_helper'
 describe Project do
 	let(:project) { FactoryGirl.create :project }
 
-	it "has a valid factory" do
+	it 'has a valid factory' do
 		expect(create(:project)).to be_valid
 	end
 
-	it "is completely filled when all fields are provided" do
+	it 'is completely filled when all fields are provided' do
 		expect(create(:project_all_fields)).to be_valid
 	end
 
-	describe "empty fields" do
-		it "is invalid when name is not provided" do
+	describe 'empty fields' do
+		it 'is invalid when name is not provided' do
 			expect(build(:project, name: nil)).to_not be_valid
 		end
 
-		it "is invalid when status is not provided" do
+		it 'is invalid when status is not provided' do
 			expect(build(:project, status: nil)).to_not be_valid
 		end
 
-		it "is invalid when priority is not provided" do
+		it 'is invalid when priority is not provided' do
 			expect(build(:project, priority: nil)).to_not be_valid
 		end
 
-		it "is valid for start date and returns 'N/A' when not provided" do
-			expect(project.start_date).to eq "N/A"
+		it 'is valid for start date and returns 'N/A' when not provided' do
+			expect(project.start_date).to eq 'N/A'
 		end
 
-		it "is valid for target date and returns 'N/A' when not provided" do
-			expect(project.target_date).to eq "N/A"
+		it 'is valid for target date and returns 'N/A' when not provided' do
+			expect(project.target_date).to eq 'N/A'
 		end
 
-		it "is valid for deadline and returns 'N/A' when not provided" do
-			expect(project.deadline).to eq "N/A"
+		it 'is valid for deadline and returns 'N/A' when not provided' do
+			expect(project.deadline).to eq 'N/A'
 		end
 
-		it "is valid for completion date and returns 'N/A' when not provided" do
-			expect(project.completion_date).to eq "N/A"
+		it 'is valid for completion date and returns 'N/A' when not provided' do
+			expect(project.completion_date).to eq 'N/A'
 		end
 
 	end
 
-	describe "field lengths" do
-    it "is invalid when name has more than 50 characters" do
+	describe 'field lengths' do
+    it 'is invalid when name has more than 50 characters' do
       expect(build(:project, name: 'a' * 51)).to_not be_valid
     end
 
-    it "is invalid when role has more than 100 characters" do
+    it 'is invalid when role has more than 100 characters' do
       expect(build(:project, role: 'a' * 101)).to_not be_valid
     end
 
-    it "is invalid when notes has more than 1000 characters" do
+    it 'is invalid when notes has more than 1000 characters' do
       expect(build(:project, notes: 'a' * 1001)).to_not be_valid
     end
 	end
 
-	describe "valid entries" do
+	describe 'valid entries' do
 		it "is valid when status is 'Not Started', 'In Progress', 'Completed'" do
 			valid_statuses = ['Not Started', 'In Progress', 'Completed']
 			valid_statuses.each do |valid_status|
@@ -64,7 +64,7 @@ describe Project do
 			end
 		end
 
-		it "is valid when priority is 'N/A', 'Low', 'High', 'Urgent'" do
+		it "is valid when priority is N/A', 'Low', 'High', 'Urgent'" do
 			valid_priorities = %w[Low High Urgent N/A]
 			valid_priorities.each do |valid_priority|
 				expect(build(:project, priority: valid_priority)).to be_valid
@@ -72,19 +72,19 @@ describe Project do
 		end
 	end
 
-	describe "invalid dates" do
-		it "cannot have a target date before start date" do
-			project = build(:project, start_date: "2013-11-02", target_date: "2013-11-01")
+	describe 'invalid dates' do
+		it 'cannot have a target date before start date' do
+			project = build(:project, start_date: '2013-11-02', target_date: '2013-11-01')
 			expect(project).to_not be_valid
 		end
 
-		it "cannot have a deadline before start date" do
-			project = build(:project, start_date: "2013-11-02", deadline: "2013-11-01")
+		it 'cannot have a deadline before start date' do
+			project = build(:project, start_date: '2013-11-02', deadline: '2013-11-01')
 			expect(project).to_not be_valid
 	end
 
-		it "cannot be a date before start date" do
-			project = build(:project, start_date: "2013-11-02", completion_date: "2013-11-01")
+		it 'cannot be a date before start date' do
+			project = build(:project, start_date: '2013-11-02', completion_date: '2013-11-01')
 			expect(project).to_not be_valid
 		end
 	end
