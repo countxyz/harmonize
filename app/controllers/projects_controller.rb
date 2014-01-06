56c26094fc6e04cc33b3f6c721019659f0834a1a
countxyz <skeptoid@gmail.com>
2014-01-06 17:50:48 -0500

    Convert indentations to spaces in .rb files.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,55 +1,55 @@
 class ProjectsController < ApplicationController
 
-	def index
-		@projects = Project.all
-	end
+  def index
+    @projects = Project.all
+  end
 
-	def show
-		@project = Project.find(params[:id])
-	end
+  def show
+    @project = Project.find(params[:id])
+  end
 
-	def new
-		@project = Project.new
-	end
+  def new
+    @project = Project.new
+  end
 
-	def create
-		@project = Project.new(project_params)
-		if @project.save
-			flash[:notice] = "Project has been created"
-			redirect_to @project
-		else
-			flash[:alert] = "Project has not been created"
-			render :action => "new"
-		end
-	end
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:notice] = "Project has been created"
+      redirect_to @project
+    else
+      flash[:alert] = "Project has not been created"
+      render :action => "new"
+    end
+  end
 
-	def edit
-		@project = Project.find(params[:id])
-	end
+  def edit
+    @project = Project.find(params[:id])
+  end
 
-	def update
-		@project = Project.find(params[:id])
-		if @project.update_attributes(project_params)
-			flash[:notice] = "Project has been updated"
-			redirect_to @project
-		else
-			flash[:alert] = "Project has not been updated"
-			render :action => "edit"
-		end
-	end
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(project_params)
+      flash[:notice] = "Project has been updated"
+      redirect_to @project
+    else
+      flash[:alert] = "Project has not been updated"
+      render :action => "edit"
+    end
+  end
 
-	def destroy
-		@project = Project.find(params[:id])
-		@project.destroy
-		flash[:notice] = "Project has been deleted"
-		redirect_to projects_path
-	end
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    flash[:notice] = "Project has been deleted"
+    redirect_to projects_path
+  end
 
-	private
+  private
 
-	def project_params
-		params.require(:project).permit(:name, :status, :priority, :role,
-																		:start_date, :target_date, :deadline,
-																		:completion_date, :notes)
-	end
+  def project_params
+    params.require(:project).permit(:name, :status, :priority, :role,
+                                    :start_date, :target_date, :deadline,
+                                    :completion_date, :notes)
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,48 +1,48 @@
 require 'spec_helper'
 
 describe Project do
-	let(:project) { FactoryGirl.create :project }
-	
-	it 'has a valid factory' do
-		expect(create(:project)).to be_valid
-	end
+  let(:project) { FactoryGirl.create :project }
+  
+  it 'has a valid factory' do
+    expect(create(:project)).to be_valid
+  end
 
-	it 'is completely filled when all fields are provided' do
-		expect(create(:project_all_fields)).to be_valid
-	end
+  it 'is completely filled when all fields are provided' do
+    expect(create(:project_all_fields)).to be_valid
+  end
 
-	describe 'empty fields' do
-		it 'is invalid when name is not provided' do
-			expect(build(:project, name: nil)).to_not be_valid
-		end
+  describe 'empty fields' do
+    it 'is invalid when name is not provided' do
+      expect(build(:project, name: nil)).to_not be_valid
+    end
 
-		it 'is invalid when status is not provided' do
-			expect(build(:project, status: nil)).to_not be_valid
-		end
+    it 'is invalid when status is not provided' do
+      expect(build(:project, status: nil)).to_not be_valid
+    end
 
-		it 'is invalid when priority is not provided' do
-			expect(build(:project, priority: nil)).to_not be_valid
-		end
+    it 'is invalid when priority is not provided' do
+      expect(build(:project, priority: nil)).to_not be_valid
+    end
 
-		it "is valid for start date and returns 'N/A' when not provided" do
-			expect(project.start_date).to eq 'N/A'
-		end
+    it "is valid for start date and returns 'N/A' when not provided" do
+      expect(project.start_date).to eq 'N/A'
+    end
 
-		it "is valid for target date and returns 'N/A' when not provided" do
-			expect(project.target_date).to eq 'N/A'
-		end
+    it "is valid for target date and returns 'N/A' when not provided" do
+      expect(project.target_date).to eq 'N/A'
+    end
 
-		it "is valid for deadline and returns 'N/A' when not provided" do
-			expect(project.deadline).to eq 'N/A'
-		end
+    it "is valid for deadline and returns 'N/A' when not provided" do
+      expect(project.deadline).to eq 'N/A'
+    end
 
-		it "is valid for completion date and returns 'N/A' when not provided" do
-			expect(project.completion_date).to eq 'N/A'
-		end
+    it "is valid for completion date and returns 'N/A' when not provided" do
+      expect(project.completion_date).to eq 'N/A'
+    end
 
-	end
+  end
 
-	describe 'field lengths' do
+  describe 'field lengths' do
     it 'is invalid when name has more than 50 characters' do
       expect(build(:project, name: 'a' * 51)).to_not be_valid
     end
@@ -54,38 +54,38 @@ describe Project do
     it 'is invalid when notes has more than 1000 characters' do
       expect(build(:project, notes: 'a' * 1001)).to_not be_valid
     end
-	end
+  end
 
-	describe 'valid entries' do
-		it "is valid when status is 'Not Started', 'In Progress', 'Completed'" do
-			valid_statuses = ['Not Started', 'In Progress', 'Completed']
-			valid_statuses.each do |valid_status|
-				expect(build(:project, status: valid_status)).to be_valid
-			end
-		end
+  describe 'valid entries' do
+    it "is valid when status is 'Not Started', 'In Progress', 'Completed'" do
+      valid_statuses = ['Not Started', 'In Progress', 'Completed']
+      valid_statuses.each do |valid_status|
+        expect(build(:project, status: valid_status)).to be_valid
+      end
+    end
 
-		it "is valid when priority is N/A', 'Low', 'High', 'Urgent'" do
-			valid_priorities = %w[Low High Urgent N/A]
-			valid_priorities.each do |valid_priority|
-				expect(build(:project, priority: valid_priority)).to be_valid
-			end
-		end
-	end
+    it "is valid when priority is N/A', 'Low', 'High', 'Urgent'" do
+      valid_priorities = %w[Low High Urgent N/A]
+      valid_priorities.each do |valid_priority|
+        expect(build(:project, priority: valid_priority)).to be_valid
+      end
+    end
+  end
 
-	describe 'invalid dates' do
-		it 'cannot have a target date before start date' do
-			project = build(:project, start_date: '2013-11-02', target_date: '2013-11-01')
-			expect(project).to_not be_valid
-		end
+  describe 'invalid dates' do
+    it 'cannot have a target date before start date' do
+      project = build(:project, start_date: '2013-11-02', target_date: '2013-11-01')
+      expect(project).to_not be_valid
+    end
 
-		it 'cannot have a deadline before start date' do
-			project = build(:project, start_date: '2013-11-02', deadline: '2013-11-01')
-			expect(project).to_not be_valid
-	end
+    it 'cannot have a deadline before start date' do
+      project = build(:project, start_date: '2013-11-02', deadline: '2013-11-01')
+      expect(project).to_not be_valid
+  end
 
-		it 'cannot be a date before start date' do
-			project = build(:project, start_date: '2013-11-02', completion_date: '2013-11-01')
-			expect(project).to_not be_valid
-		end
-	end
+    it 'cannot be a date before start date' do
+      project = build(:project, start_date: '2013-11-02', completion_date: '2013-11-01')
+      expect(project).to_not be_valid
+    end
+  end
 end
