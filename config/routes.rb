6d1414f70d51f39a053b0b4c72ab8983930b84b3
countxyz <skeptoid@gmail.com>
2014-02-21 18:22:04 -0500

    Install dashing-rails dependencies.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Harmonize::Application.routes.draw do
 
+  mount Dashing::Engine, at: Dashing.config.engine_path
   root :to => 'dashboard#index'
   match '/about', to: 'main_pages#about', via: 'get'
 
