c0346108c69ce17c8051f4e3d8087b386d506359
countxyz <skeptoid@gmail.com>
2014-02-21 05:06:25 -0500

    Remove spork configurations.

--- Guardfile
+++ Guardfile
@@ -1,12 +1,3 @@
-guard 'spork', :rspec_env => { 'RAILS_ENV' => 'test' } do
-  watch('config/application.rb')
-  watch('config/environment.rb')
-  watch('config/environments/test.rb')
-  watch(%r{^config/initializers/.+\.rb$})
-  watch('Gemfile.lock')
-  watch('spec/spec_helper.rb') { :rspec }
-end
-
 guard :rspec, cmd: "rspec" do
   watch(%r{^spec/.+_spec\.rb$})
   watch(%r{^lib/(.+)\.rb$})     { |m| "spec/#{m[1]}_spec.rb" }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,36 +1,30 @@
 require 'rubygems'
-require 'spork'
 
-Spork.prefork do
-  ENV["RAILS_ENV"] ||= 'test'
-  require File.expand_path("../../config/environment", __FILE__)
-  require 'rspec/rails'
-  require 'rspec/autorun'
-  require 'capybara/rails'
-  require 'capybara/rspec'
-  require 'database_cleaner'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+require 'capybara/rails'
+require 'capybara/rspec'
+require 'database_cleaner'
 
-  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
-  ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
-  RSpec.configure do |config|
-    config.use_transactional_fixtures = false
-    config.infer_base_class_for_anonymous_controllers = false
-    config.order = "random"
-    config.include FactoryGirl::Syntax::Methods
-    config.before(:suite) do
-      DatabaseCleaner.strategy = :truncation
-    end
-
-    config.before(:each) do
-      DatabaseCleaner.start
-    end
-
-    config.after(:each) do
-      DatabaseCleaner.clean
-    end
+RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+  config.infer_base_class_for_anonymous_controllers = false
+  config.order = "random"
+  config.include FactoryGirl::Syntax::Methods
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
   end
-end
 
-Spork.each_run do
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
