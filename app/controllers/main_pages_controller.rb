f545cf45773db99db67d2a68f203958c161fdf47
countxyz <skeptoid@gmail.com>
2014-02-21 18:11:27 -0500

    Add copyright notice to site in about page.

---
+++ app/controllers/main_pages_controller.rb
@@ -0,0 +1,5 @@
+class MainPagesController < ApplicationController
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Harmonize::Application.routes.draw do
 
   root :to => 'dashboard#index'
+  match '/about', to: 'main_pages#about', via: 'get'
 
   resources :projects, :contacts, :events
 end
