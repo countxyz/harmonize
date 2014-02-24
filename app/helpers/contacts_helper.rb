9bad0806127a4c706a0a658cb5a06923551189d9
countxyz <skeptoid@gmail.com>
2014-02-24 10:37:04 -0500

    Refactor phone format in Contacts helper.

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,8 +1,8 @@
 module ContactsHelper
 
   def phone_format(phone)
-    if phone == nil || phone.empty?
-      phone = "N/A"
+    if phone.nil?
+      'N/A'
     else
       number_to_phone(phone, area_code: true)
     end
