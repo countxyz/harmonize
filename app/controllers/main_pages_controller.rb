6819d883cf44f0d939ea6debca7c8cc1bd10865a
countxyz <skeptoid@gmail.com>
2014-02-28 22:21:16 -0500

    Add date to home.

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -1,5 +1,10 @@
 class MainPagesController < ApplicationController
 
+  def home
+    @time = Time.new
+    @day = @time.strftime("%A")
+    @date = @time.strftime("%B %e, %Y")
+  end
+
   def about; end
-  def home; end
 end
