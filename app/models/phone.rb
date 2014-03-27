7791173645bc799ce72f391c968fafaa80933fca
countxyz <skeptoid@gmail.com>
2014-03-27 01:28:43 -0400

    Fix stylesheets for form.

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,6 +1,7 @@
 class Phone < ActiveRecord::Base
   belongs_to :contact, inverse_of: :phone 
 
-  validates :office, :mobile, :fax, :home, numericality: { only_integer: true },
-            length: { is: 10 }, uniqueness: { scope: :contact }
+  validates :office, :mobile, :fax, :home, presence: false, allow_blank: true,
+    numericality: { only_integer: true }, length: { is: 10 },
+    uniqueness: { scope: :contact }
 end
