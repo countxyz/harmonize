e4a551a5095b49535da574f9517556cea374a4f5
countxyz <skeptoid@gmail.com>
2014-04-05 20:55:42 -0400

    Install and configure devise.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,10 +4,13 @@ Harmonize::Application.configure do
   config.eager_load = false
   config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
   config.action_mailer.raise_delivery_errors = false
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
   config.assets.debug = true
 
-  Paperclip.options[:command_path] = "/usr/bin/convert"
+  Paperclip.options[:command_path] = '/usr/bin/convert'
 end
