10a4f3ee2e7daa5f63e819cc03899fd73ad492e2
countxyz <skeptoid@gmail.com>
2014-04-16 13:27:50 -0400

    Create Users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140416170645) do
+ActiveRecord::Schema.define(version: 20140416172123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,5 +88,13 @@ ActiveRecord::Schema.define(version: 20140416170645) do
   end
 
   add_index "tasks", ["user_id"], name: "index_tasks_on_user_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "handle"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
