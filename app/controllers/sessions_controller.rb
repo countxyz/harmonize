a481b7462ed0357a03331377168ee007694ecc0a
countxyz <skeptoid@gmail.com>
2014-04-25 06:48:04 -0400

    Add notice when user signs in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:signin][:password])
       session[:user_id] = user.id
+      flash[:notice] = 'Sign in successful'
       redirect_to root_url
     else
       flash[:error] = 'Unsuccessful sign in'
