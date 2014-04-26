cd85ffc7664f685117622026a919ef7430adc276
countxyz <skeptoid@gmail.com>
2014-04-26 11:12:32 -0400

    Make prettier submit buttons.

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -14,7 +14,7 @@ SimpleForm.setup do |config|
 
   config.default_wrapper = :default
   config.boolean_style = :nested
-  config.button_class = 'btn'
+  config.button_class = 'submit'
   config.error_notification_tag = :div
   config.error_notification_class = 'alert alert-error'
   config.label_class = 'control-label'
