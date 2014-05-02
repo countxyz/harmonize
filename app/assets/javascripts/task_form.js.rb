02a712cddc13f52c79d0178688fe6693f6368e87
countxyz <skeptoid@gmail.com>
2014-05-02 11:58:44 -0400

    Style pending tasks.

--- app/assets/javascripts/task_form.js.rb
+++ app/assets/javascripts/task_form.js.rb
@@ -3,7 +3,7 @@ require 'opal-jquery'
 class TaskForm
 
   def new_task
-    Element.find '#new_task'
+    Element.find('#new-task')
   end
 
   def edit_task
@@ -15,11 +15,11 @@ class TaskForm
   end
 
   def fade_in_new_task_form
-    Element.find('#new_task_form').effect :fade_in
+    Element.find('#new-task-form').effect :fade_in
   end
 
   def fade_out_new_task_form
-    Element.find('#new_task_form').effect :fade_out
+    Element.find('#new-task-form').effect :fade_out
   end
 
   def show_task_form_new
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -3,7 +3,7 @@ module TasksHelper
   def datetime_format(datetime)
     if datetime.nil? then 'No Deadline'
     else
-      datetime.strftime('%B %d, %Y - %A - %l:%M %P %Z')
+      datetime.strftime('%B %d, %Y - %A %l:%M %P %Z')
     end
   end
 end
