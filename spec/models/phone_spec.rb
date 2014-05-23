8cd922f875535dfe5cd21fdeab53c459138f13f7
countxyz <skeptoid@gmail.com>
2014-05-23 10:37:28 -0400

    Refactor phone specs.

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -1,30 +1,24 @@
 require 'spec_helper'
 
 describe Phone do
-  it 'does not allow duplicate phone numbers per contact' do
-    expect(build(:phone, office: '9175555555')).to_not be_valid
-  end
+  it { should validate_uniqueness_of(:office).scoped_to(:contact_id) }
 
   it 'allows two contacts to share a phone number' do
     contact_1 = create(:contact)
     expect(build(:other_contact)).to be_valid
   end
 
-  describe 'format' do
-    it 'is invalid without 10 numbers' do
-      expect(build(:phone, office: '21255')).to_not be_valid
-    end
+  describe 'field lengths' do
+    it { should ensure_length_of(:office).is_equal_to(10) }
+    it { should ensure_length_of(:mobile).is_equal_to(10) }
+    it { should ensure_length_of(:fax).is_equal_to(10)    }
+    it { should ensure_length_of(:home).is_equal_to(10)   }
+  end
 
-    it 'is invalid without 10 numbers' do
-      expect(build(:phone, mobile: '212555555')).to_not be_valid
-    end
-
-    it 'is invalid without 10 numbers' do
-      expect(build(:phone, fax: '5555555')).to_not be_valid
-    end
-
-    it 'is invalid without 10 numbers' do
-      expect(build(:phone, home: '5')).to_not be_valid
-    end
+  describe 'field characters' do
+    it { should validate_numericality_of(:office).only_integer }
+    it { should validate_numericality_of(:mobile).only_integer }
+    it { should validate_numericality_of(:fax).only_integer    }
+    it { should validate_numericality_of(:home).only_integer   }
   end
 end
