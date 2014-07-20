4935b7fdda0ee2eb8002c69dcedc538eae0a4553
countxyz <skeptoid@gmail.com>
2014-07-19 21:45:43 -0400

    Refactor profile updater.

--- app/assets/javascripts/profile_updater.js.rb
+++ app/assets/javascripts/profile_updater.js.rb
@@ -2,39 +2,40 @@ require 'opal-jquery'
 
 class ProfileUpdater
 
-  def table_to_form panel, table, form
-    Element.find(panel).on :click do |event|
-      event.prevent_default
-      Element.find(table).hide
-      Element.find(form).effect :slide_down
-    end
-  end
-
-  def form_to_table close, table, form
-    Element.find(close).on :click do |event|    
-      event.prevent_default
-      Element.find(form).hide
-      Element.find(table).effect(:slide_down, 200)
-    end
-  end
-
-  def update_profile panel, table, form, close
+  def update_profile(css)
     Document.on 'page:change' do
-      table_to_form panel, table, form
-      form_to_table close, table, form
+      table_to_form(css)
+      form_to_table(css)
     end
   end
+
+  private
+
+    def table_to_form(css)
+      Element.find("##{css}-panel").on :click do |click|
+        click.prevent_default
+        slide_down_form(css)
+      end
+    end
+
+    def slide_down_form(css)
+      Element.find("##{css}-table").hide
+      Element.find("#edit-#{css}-form").effect :slide_down
+    end
+
+    def form_to_table(css)
+      Element.find("#close-#{css}").on :click do |event|
+        event.prevent_default
+        slide_up_form(css)
+      end
+    end
+
+    def slide_up_form(css)
+      Element.find("#edit-#{css}-form").hide
+      Element.find("##{css}-table").effect(:slide_down, 200)   
+    end 
 end
 
-contact = ProfileUpdater.new
-
-contact.update_profile('#ci-panel', '#ci-table', '#edit-ci-form', '#close-ci')
-contact.update_profile('#pb-panel', '#pb-table', '#edit-pb-form', '#close-pb')
-contact.update_profile('#sm-panel', '#sm-table', '#edit-sm-form', '#close-sm')
-
-account = ProfileUpdater.new
-
-account.update_profile('#ai-panel', '#ai-table', '#edit-ai-form', '#close-ai')
-account.update_profile('#ba-panel', '#ba-table', '#edit-ba-form', '#close-ba')
-account.update_profile('#sa-panel', '#sa-table', '#edit-sa-form', '#close-sa')
-account.update_profile('#em-panel', '#em-table', '#edit-em-form', '#close-em')
+click_edit = ProfileUpdater.new
+css_ids = ['ai', 'pb', 'em', 'ba', 'sa', 'sm']
+css_ids.each { |css_id| click_edit.update_profile(css_id) }
