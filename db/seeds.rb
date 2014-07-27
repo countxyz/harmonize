6f937d114186ebee3dd851fc9dd22e3adc0ce281
countxyz <skeptoid@gmail.com>
2014-07-27 00:43:30 -0400

    Provide ability to seed account data with addresses. Slam renegade samples into methods.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,48 @@
+def boolean_sample
+  [true, true, false].sample
+end
+
+def street_number_sample
+  Random.new.rand(10..9999)
+end
+
+def road_type_sample
+  %w(St. Blvd. Ave.).sample
+end
+
+def city_sample
+  %w(Greenville Washington Franklin Fairview Springfield Dongan).sample
+end
+
+def composed_street_address
+  "#{street_number_sample} #{Faker::Name.last_name} #{road_type_sample}"
+end
+
 User.destroy_all
 
-User.create!(handle:                'demo_user',
-             password:              'harmonize',
-             password_confirmation: 'harmonize',
-             admin:                 false)
+User.create!(handle: 'demo_user', password: 'harmonize',
+  password_confirmation: 'harmonize', admin: false)
 
-demo_admin = User.create!(handle:                'demo_admin',
-                          first_name:            'Phil',
-                          last_name:             'Collins',
-                          password:              'harmonize',
-                          password_confirmation: 'harmonize',
-                          admin:                 true)
+demo_admin = User.create!(handle: 'demo_admin', first_name: 'Phil', admin: true,
+  last_name: 'Collins', password: 'harmonize', password_confirmation: 'harmonize')
 
-hlnews =     User.create!(handle:                'hlnews',
-                          first_name:            'Huey',
-                          last_name:             'Lewis',
-                          password:              'harmonize',
-                          password_confirmation: 'harmonize',
-                          admin:                 true)
+hlnews = User.create!(handle: 'hlnews', first_name: 'Huey', last_name: 'Lewis',
+  password: 'harmonize', password_confirmation: 'harmonize', admin: true)
 
 demo_admin.save!
 hlnews.save!
 
 Account.destroy_all
-
-random_admin = [demo_admin.id, hlnews.id].sample
-random_boolean = [true, false].sample
+Address.destroy_all
 
 40.times do
-  Account.create!(
-    name: Faker::Company.name,
-    active: random_boolean,
-    user_id: random_admin)
+  account = Account.create!(name: Faker::Company.name, active: boolean_sample,
+    website: Faker::Internet.domain_name, user_id: [demo_admin.id, demo_admin.id, hlnews.id].sample,
+    created_at: Faker::Time.date)
+
+  account.save!
+
+  Address.create!(street_1: composed_street_address, city: city_sample,
+    state: Faker::AddressUS.state_abbr, postal_code: Faker::AddressUS.zip_code,
+    country: 'US', type: 'BillingAddress', addressable_id: account.id)
 end
