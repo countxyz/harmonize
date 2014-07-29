4c939d3073a7432b565dfeb84d46ee68111d272b
countxyz <skeptoid@gmail.com>
2014-07-29 13:22:30 -0400

    Add task format helper to application helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,12 @@ module ApplicationHelper
 
   def full_title(page_title)
     base_title
-    if page_title.empty? then base_title
-    else "#{base_title} | #{page_title}"; end
+
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
   end
 
   def base_title
@@ -30,14 +34,8 @@ module ApplicationHelper
     'danger' unless account.active?
   end
 
-  def icon(icon, text="", html_options={})
-    content_class = "fa fa-#{icon}"
-    content_class << " #{html_options[:class]}" if html_options.key?(:class)
-    html_options[:class] = content_class
-
-    html = content_tag(:i, nil, html_options)
-    html << " #{text}" unless text.blank?
-    html.html_safe
+  def non_programmer_format(counter)
+    counter + 1
   end
 
   def logged_in_format(current_user)
@@ -46,5 +44,23 @@ module ApplicationHelper
     else
       "Logged in as User: #{current_user.handle}"
     end
+  end
+
+  def task_datetime_format(datetime)
+    if datetime.nil?
+      'No Deadline'
+    else
+      datetime.strftime('%B %e, %Y %l:%M %P %Z')
+    end
+  end
+
+  def icon(icon, text="", html_options={})
+    content_class = "fa fa-#{icon}"
+    content_class << " #{html_options[:class]}" if html_options.key?(:class)
+    html_options[:class] = content_class
+
+    html = content_tag(:i, nil, html_options)
+    html << " #{text}" unless text.blank?
+    html.html_safe
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -49,4 +49,24 @@ describe ApplicationHelper do
       expect(helper.inactive_danger_tr(account)).to eq 'danger'
     end
   end
+
+  describe 'Datetime Format' do
+    it "is labeled with 'No Deadline' when datetime not given" do
+      datetime = nil
+      expect(helper.task_datetime_format datetime).to eql 'No Deadline'
+    end
+
+    it 'formats datetime when given' do
+      datetime = DateTime.new(2014, 7, 31, 9, 0, 0)
+      expect(helper.task_datetime_format datetime).to be
+        'July 31, 2014 9:00 am EDT'
+    end
+  end
+
+  describe 'non-programmer format' do
+    it 'displays counter starting from 1' do
+      counter = 0
+      expect(helper.non_programmer_format(counter)).to eq 1
+    end
+  end
 end
